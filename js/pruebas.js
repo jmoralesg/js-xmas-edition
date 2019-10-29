4edcd03be12865ef00d4ac494805bb26b0813b24
jmoralesg <johanamgff@gmail.com>
2019-10-29 13:05:31 +1100

    solucion validaciones tarea1

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,15 +1,43 @@
 function probarValidarNombre() {
   console.assert(
-      validarNombre('') === 'Este campo debe tener al menos 1 caracter',
-      'Validar nombre no validó que el nombre no sea vacío',
+    validarNombre('') === 'Este campo debe tener al menos 1 caracter',
+    'Validar nombre no validó que el nombre no sea vacío'
   );
 
   console.assert(
-      validarNombre(
-          '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') ===
-      'Este campo debe tener menos de 50 caracteres',
-      'Validar nombre no validó que el nombre sea menor a 50 caracteres',
+    validarNombre(
+      '111111111111111111111111111111111111111111111111111111111111111111111111111111111111111'
+    ) === 'Este campo debe tener menos de 50 caracteres',
+    'Validar nombre no validó que el nombre sea menor a 50 caracteres'
+  );
+}
+
+function probarValidarCiudad() {
+  console.assert(
+    validarCiudad('') ===
+      'Este campo no es valido, al menos una ciudad debe ser seleccionada',
+    'validar ciudad no funciono con un valor vacio'
+  );
+
+  console.assert(
+    validarCiudad('Chaco') === '',
+    'validar ciudad falla al seleccionar una ciudad'
+  );
+}
+
+function probarValidarDescripcionRegalo() {
+  console.assert(
+    validarDescripcionRegalo('') ===
+      'Este campo no es valido, ingrese una descripcion',
+    'validar descripcion de regalo falla que no sea vacio'
+  );
+
+  console.assert(
+    validarDescripcionRegalo('bici') === '',
+    'validar descripcion ingresada falla'
   );
 }
 
 probarValidarNombre();
+probarValidarCiudad();
+probarValidarDescripcionRegalo();
--- tarea/tarea-clase-7.js
+++ tarea/tarea-clase-7.js
@@ -5,3 +5,37 @@
 * Adicional: Escribir pruebas para las funciones de tareas anteriores.
 *
 * */
+const $form = document.querySelector('#carta-a-santa');
+const nombre = $form.nombre.value;
+const ciudad = $form.ciudad.value;
+const comportamiento = $form.comportamiento.value;
+const descripcion = $form['descripcion-regalo'].value;
+
+function validarNombre(nombre) {
+  if (nombre.length === 0) {
+    return 'Este campo debe tener al menos 1 caracter';
+  }
+  if (nombre.length >= 50) {
+    return 'Este campo debe tener menos de 50 caracteres';
+  }
+
+  return '';
+}
+
+function validarCiudad(ciudad) {
+  if (ciudad.length === 0) {
+    return 'Este campo no es valido, al menos una ciudad debe ser seleccionada';
+  }
+  if (ciudad === 'Chaco') {
+    return '';
+  }
+}
+
+function validarDescripcionRegalo(descripcion) {
+  if (descripcion.lenght === 0) {
+    return 'Este campo no es valido, ingrese una descripcion';
+  }
+  if (descripcion === 'bici') {
+    return '';
+  }
+}
