2877aa1f6992a9da88a3791ab5df434a0c5997ac
jmoralesg <johanamgff@gmail.com>
2019-11-13 12:58:48 +1100

    validaciones completadas

--- js/pruebas.js
+++ js/pruebas.js
@@ -10,31 +10,52 @@ function probarValidarNombre() {
     ) === 'Este campo debe tener menos de 50 caracteres',
     'Validar nombre no validó que el nombre sea menor a 50 caracteres'
   );
+
+  console.assert(
+    validarNombre('123') === 'El campo nombre solo acepta letras',
+    'Nombre no valido que el nombre solo tenga letras'
+  );
+
+  console.assert(
+    validarNombre('Johana') === '',
+    'validarNombre fallo con un nombre valido'
+  ); // prueba el caso cuando la funcion funciona
 }
 
 function probarValidarCiudad() {
   console.assert(
+    validarCiudad('Buenos Aires') === '',
+    'validar ciudad no funciono con nombre de ciudad valido'
+  );
+  console.assert(
     validarCiudad('') ===
       'Este campo no es valido, al menos una ciudad debe ser seleccionada',
-    'validar ciudad no funciono con un valor vacio'
-  );
-
-  console.assert(
-    validarCiudad('') === '',
-    'validar ciudad falla al seleccionar una ciudad'
+    'validar ciudad no mostro un error cuando la ciudad es vacia'
   );
 }
 
 function probarValidarDescripcionRegalo() {
   console.assert(
     validarDescripcionRegalo('') ===
-      'Este campo no es valido, ingrese una descripcion',
-    'validar descripcion de regalo falla que no sea vacio'
+      'Este campo descripcion no puede estar vacio',
+    'La funcion validar descripcion regalo no valido que el campo no este vacio'
+  );
+  console.assert(
+    validarDescripcionRegalo(
+      'dfgdfgdfdfgdsgdfgdfgdsfgdsfgsdfgsdfgdsfgsdfgsdgdsfgdfgfghfghgfjyjdynjdtnsgsdfgsdfgdgfsdgdsfgdfsgsdfgdfsgdfg'
+    ) === 'Este campo descripcion es muy largo',
+    'La funcion validar regalo no valido que el campo no sea muy largo '
   );
 
   console.assert(
-    validarDescripcionRegalo('') === '',
-    'validar descripcion ingresada falla'
+    validarDescripcionRegalo('Regalo') === '',
+    'La funcion validar regalo no funciono con una descripcion correcta'
+  );
+
+  console.assert(
+    validarDescripcionRegalo(',.,,..') ===
+      'El campo descripcion solo puede tener numeros y letras',
+    'Esta funcion fallo que la descripcion no valido que fuera solo numeros y letras'
   );
 }
 
--- tarea/tarea-clase-7.js
+++ tarea/tarea-clase-7.js
@@ -1,41 +1,33 @@
-/*
- * Hacer las funciones de validación de validarCiudad y validarDescripcionRegalo.
- * Escribir pruebas para esas funciones.
- *
- * Adicional: Escribir pruebas para las funciones de tareas anteriores.
- *
- * */
-const $form = document.querySelector('#carta-a-santa');
-const nombre = $form.nombre.value;
-const ciudad = $form.ciudad.value;
-const comportamiento = $form.comportamiento.value;
-const descripcion = $form['descripcion-regalo'].value;
-
 function validarNombre(nombre) {
   if (nombre.length === 0) {
-    return 'Este campo debe tener al menos 1 caracter';
+    return "Este campo debe tener al menos 1 caracter";
   }
   if (nombre.length >= 50) {
-    return 'Este campo debe tener menos de 50 caracteres';
+    return "Este campo debe tener menos de 50 caracteres";
   }
 
-  return '';
+  if (!/^[a-z]+$/i.test(nombre)) {
+    return "El campo nombre solo acepta letras";
+  }
+  return "";
 }
 
 function validarCiudad(ciudad) {
   if (ciudad.length === 0) {
-    return 'Este campo no es valido, al menos una ciudad debe ser seleccionada';
+    //tambien es permitido ciudad === ''
+    return "Este campo no es valido, al menos una ciudad debe ser seleccionada";
   }
-  if (ciudad === '') {
-    return '';
-  }
+  return "";
 }
 
 function validarDescripcionRegalo(descripcion) {
-  if (descripcion.lenght === 0) {
-    return 'Este campo no es valido, ingrese una descripcion';
-  }
-  if (descripcion === '') {
-    return '';
+  if (descripcion.length >= 100) {
+    return "Este campo descripcion es muy largo";
+  } else if (descripcion.length === 0) {
+    return "Este campo descripcion no puede estar vacio";
+  } else if (!/^[a-z0-9 ]+$/i.test(descripcion)) {
+    return "El campo descripcion solo puede tener numeros y letras";
+  } else {
+    return "";
   }
 }
